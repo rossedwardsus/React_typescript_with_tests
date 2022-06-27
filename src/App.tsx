935446178047f26e60fd131cdffcb05d386884cc
Ross Edwards <rossedwards.us@gmail.com>
2022-06-27 12:10:05 -0400

    added recoil

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import { Routes, Route, Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import "bootstrap/dist/css/bootstrap.css"
+import { atom, selector, useRecoilState, useRecoilValue } from "recoil";
 import './App.css';
+
+
+export interface Bookmark {
+  id?: string;
+  title: string;
+  completed: boolean;
+}
+
+export const bookmarksState = atom({
+  key: "bookmarks",
+  default: [] as Bookmark[],
+});
+
+export const infoValue = selector({
+  key: "infoValue",
+  get: ({ get }) => ({
+    total: get(bookmarksState).length,
+    completed: get(bookmarksState).filter((bookmark) => bookmark.completed).length,
+    notCompleted: get(bookmarksState).filter((bookmark) => !bookmark.completed).length,
+  }),
+});
 
 function Home() {
   return (
@@ -56,8 +79,9 @@ function Bookmarks() {
         Tags
       </main>
       <nav>
-        <Link to="/about">About</Link>
-        <Link to="/bookmarks">Bookmarks</Link>
+        <Link to="/add_bookmark_group">Add Bookmark Group</Link>
+        <br/>
+        <Link to="/add_bookmark_tag">Add Bookmark Tag</Link>
       </nav>
       <br/>
       Your bookmarks
@@ -69,6 +93,7 @@ function Bookmarks() {
 function AddBookmarkGroup() {
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
   const onSubmit = (data: any) => console.log(data);
+  const [bookmarkss, seookmarkss] = useRecoilState(bookmarksState);
 
   return (
     <>
@@ -80,6 +105,19 @@ function AddBookmarkGroup() {
         Bookmark groups
         <br/>
         Tags
+        <br/>
+        <br/>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* register your input into the hook by invoking the "register" function */}
+          <input defaultValue="test" {...register("group_name")} />
+          
+          {/* include validation with required or other standard HTML validation rules */}
+          <input {...register("exampleRequired", { required: true })} />
+          {/* errors will return when field validation fails  */}
+          {errors.exampleRequired && <span>This field is required</span>}
+          
+          <input type="submit" />
+        </form>
       </main>
       <nav>
         <Link to="/about">About</Link>
@@ -163,7 +201,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="bookmarks" element={<Bookmarks />} />
-        <Route path="add_bookmark_groups" element={<AddBookmarkGroup />} />
+        <Route path="add_bookmark_group" element={<AddBookmarkGroup />} />
         <Route path="add_bookmark_tags" element={<AddBookmarkTags />} />
         <Route path="add_bookmark" element={<AddBookmark />} />
       </Routes>
